761ee9ba3023d0308462720bd8c5cd5d805b4afc
Ryan Monteiro <ryanmonteiro@outlook.com>
2023-11-04 17:12:48 -0300

    Alterções no index.html e main.js utilizando a função map para dimunir a verbosidade.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,27 @@
+const offset = 0
+const limit = 10
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-console.log('Sucesso!')
+function convertPokemonToLi(pokemon) {
+    return `
+        <li class="pokemon">
+            <span class="number">#001</span>
+            <span class="name">${pokemon.name}</span>
+
+            <div class="detail">
+                <ol class="types">
+                    <li class="type">Grass</li>
+                    <li class="type">Poison</li>
+                </ol>
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+                    alt="${pokemon.name}">
+            </div> 
+        </li>
+    `
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
+pokeApi.getPokemon().then((pokemons = []) => {
+    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+})
