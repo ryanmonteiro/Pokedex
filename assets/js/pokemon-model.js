03b6aebf77713d6287cca27e45bd5d222c2d9ced
Ryan Monteiro <ryanmonteiro@outlook.com>
2023-11-05 08:53:49 -0300

    Inclusão da pagina de status e informções dos Pokemons

--- assets/js/pokemon-model.js
+++ assets/js/pokemon-model.js
@@ -1,8 +1,20 @@
-
-class Pokemon{
+class Pokemon {
     number;
     name;
     type;
     types = [];
     photo;
+    baseExp;
+    height;
+    weight;
+    abilities = [];
+    moves = [];
+    stats = [];
+    description;
+    generation;
+    eggGroups = [];
+    evolutionChain;
+    evolutions = [];
+    evolutionsSprites = [];
+    liked;
 }
